9c349780652eef18ecd7167bfd1970b43e362119
jingyip <panjingyi0824@gmail.com>
2020-05-16 01:19:33 -0700

    homepage controller1

---
+++ src/main/java/com/example/restingspace/controller/HomePageController.java
@@ -0,0 +1,28 @@
+package com.example.restingspace.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomePageController {
+    @GetMapping("/index")
+    public String sayIndex(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(@RequestParam(value="error", required =false) String error,
+                        @RequestParam(value="logout",required = false) String logout, Model model){
+        if(error!=null){
+            model.addAttribute("error", "InValid username and password");
+        }
+        if(logout!=null){
+            model.addAttribute("logout", "You have logged out successfully");
+        }
+        return "login";
+    }
+
+}

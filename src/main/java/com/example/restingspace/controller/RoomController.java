b74828435d607b0c8ac7fab4a4edd84490b54e72
kennHub <56400125+kennHub@users.noreply.github.com>
2020-05-30 10:50:24 -0700

    response type changed to ResponseEntity<>

--- src/main/java/com/example/restingspace/controller/RoomController.java
+++ src/main/java/com/example/restingspace/controller/RoomController.java
@@ -3,6 +3,9 @@ package com.example.restingspace.controller;
 import com.example.restingspace.model.Room;
 import com.example.restingspace.service.RoomService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -10,7 +13,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,27 +26,22 @@ public class RoomController {
     @Autowired
     private RoomService roomService;
     @RequestMapping(value = "/getAllRooms", method = RequestMethod.GET)
-    public ModelAndView getAllRooms() {
+    public ResponseEntity<List<Room>> getAllRooms() {
         List<Room> rooms = roomService.getAllRooms();
-        return new ModelAndView("roomList", "rooms", rooms);
+        return new ResponseEntity<>(rooms, new HttpHeaders(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/getRoom/{rid}", method = RequestMethod.GET)
-    public ModelAndView getRoom(@PathVariable(value = "rid") int rid) {
+    public ResponseEntity<Room> getRoom(@PathVariable(value = "rid") int rid) {
         Room room = RoomService.getRoom(rid);
-        return new ModelAndView("roomPage", "room", room);
-    }
-
-    @RequestMapping(value = "/admin/room/addRoom", method = RequestMethod.GET)
-    public ModelAndView getRoomForm() {
-        return new ModelAndView("addRoom", "roomForm", new Room());
+        return new ResponseEntity<>(room, new HttpHeaders(), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/admin/room/addRoom", method = RequestMethod.POST)
-    public String addRoom(@ModelAttribute(value = "roomForm") Room room, BindingResult result) {
+    public void addRoom(@ModelAttribute(value = "roomForm") Room room, BindingResult result) {
 
         if (result.hasErrors()) {
-            return "addRoom";
+            return;
         }
         roomService.addRoom(room);
         MultipartFile image = room.getRoomImage();
@@ -53,25 +50,21 @@ public class RoomController {
             Path path = Paths.get("/Users/xxx/rooms/" + room.getRid() + ".jpg");
 
             // windows
-//			Path path = Paths.get("C:\\products\\" + product.getId() + ".jpg");
+//			Path path = Paths.get("C:\\rooms\\" + room.getRid() + ".jpg");
             try {
                 image.transferTo(new File(path.toString()));
             } catch (IllegalStateException | IOException e) {
                 e.printStackTrace();
             }
         }
-
-        return "redirect:/getAllRooms";
     }
 
     @RequestMapping(value = "/admin/delete/{rid}")
-    public String deleteRoom(@PathVariable(value = "rid") int rid) {
-
+    public void deleteRoom(@PathVariable(value = "rid") int rid) {
         // for MAC
         Path path = Paths.get("/Users/xxx/rooms/" + rid + ".jpg");
-
         // For windows
-        //Path path = Paths.get("C:\\products\\" + productId + ".jpg");
+        //Path path = Paths.get("C:\\rooms\\" + rid + ".jpg");
 
         if (Files.exists(path)) {
             try {
@@ -80,27 +73,13 @@ public class RoomController {
                 e.printStackTrace();
             }
         }
-
         roomService.deleteRoom(rid);
-        return "redirect:/getAllRooms";
-    }
-
-    @RequestMapping(value = "/admin/room/updateRoom/{rid}", method = RequestMethod.GET)
-    public ModelAndView getEditForm(@PathVariable(value = "rid") int rid) {
-        Room room = RoomService.getRoom(rid);
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("updateRoom");
-        modelAndView.addObject("updateRoomObj", room);
-        modelAndView.addObject("rid", rid);
-
-        return modelAndView;
     }
 
     @RequestMapping(value = "/admin/room/updateRoom/{rid}", method = RequestMethod.POST)
-    public String updateRoom(@ModelAttribute(value = "updateRoomObj") Room room,
+    public void updateRoom(@ModelAttribute(value = "updateRoomObj") Room room,
                            @PathVariable(value = "rid") int rid) {
         room.setRid(rid);
         roomService.updateRoom(room);
-        return "redirect:/getAllRooms";
     }
 }

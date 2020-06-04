746d525d2dd331df874c007f8c57e264f135e755
Yuchao Zhang <35940382+yc450894425@users.noreply.github.com>
2020-06-04 12:45:00 -0400

    June 4 by Yuchao Zhang

--- src/main/java/com/example/restingspace/Dao/UserDao.java
+++ src/main/java/com/example/restingspace/Dao/UserDao.java
@@ -1,10 +1,13 @@
 package com.example.restingspace.Dao;
 
 import com.example.restingspace.model.User;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -16,19 +19,33 @@ public class UserDao {
     @Autowired
     private SessionFactory sessionFactory;
 
-    public void addUser(User user) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.save(user);
-        session.getTransaction().commit();
-        if (session != null) {
-            session.close();
+    public void addUser(User user){
+        User isExisted = getUserByUsername(user.getUsername());
+        if (isExisted != null){
+            throw new ResponseStatusException(
+                    HttpStatus.IM_USED, "This username is already used."
+            );
+        }
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
     public User getUserByUsername(String username) {
         User user = null;
-        try (Session session = sessionFactory.openSession()) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
             session.beginTransaction();
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
@@ -38,13 +55,19 @@ public class UserDao {
             session.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
         return user;
     }
 
-    public List<User> getAllUsers(){
+    public List<User> getAllUsers() {
         List<User> result = null;
-        try (Session session = sessionFactory.openSession()) {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
             session.beginTransaction();
             CriteriaBuilder builder = session.getCriteriaBuilder();
             CriteriaQuery<User> criteriaQuery = builder.createQuery(User.class);
@@ -54,6 +77,10 @@ public class UserDao {
             session.getTransaction().commit();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
         return result;
     }

d24119d813704b1b26827eee48a21d65bf83d9c4
Yuchao Zhang <35940382+yc450894425@users.noreply.github.com>
2020-06-03 21:29:35 -0400

    June 3 by Yuchao Zhang

--- src/main/java/com/example/restingspace/controller/HomePageController.java
+++ src/main/java/com/example/restingspace/controller/HomePageController.java
@@ -12,16 +12,4 @@ public class HomePageController {
     public String sayIndex(){
         return "index";
     }
-
-//    @RequestMapping("/login")
-//    public String login(@RequestParam(value="error", required =false) String error,
-//                        @RequestParam(value="logout",required = false) String logout, Model model){
-//        if(error!=null){
-//            model.addAttribute("error", "InValid username and password");
-//        }
-//        if(logout!=null){
-//            model.addAttribute("logout", "You have logged out successfully");
-//        }
-//        return "login";
-//    }
 }
